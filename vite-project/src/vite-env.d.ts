0165ffdf54ebdb88035dabeccda6f10fbcab32a7
englan.muca <129820750+EnglanM@users.noreply.github.com>
2023-11-30 17:48:55 +0100

    implemented recast& created a navmesh

--- vite-project/src/vite-env.d.ts
+++ vite-project/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+// vite.config.js
+
+// export default {
+//     build: {
+//       rollupOptions: {
+//         // Exclude "recast-navigation" from pre-bundling
+//         external: ['recast-navigation'],
+//       },
+//     },
+//   };
+  
